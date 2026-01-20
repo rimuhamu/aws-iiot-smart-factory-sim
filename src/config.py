99efa88bb6536bb6fc80fd7135d91aaaf8d728c2
Emir Risyad <emirrsyd@gmail.com>
2026-01-20 22:28:10 +0100

    feat(config): implement centralized settings for AWS endpoints and simulation parameters

---
+++ src/config.py
@@ -0,0 +1,8 @@
+ENDPOINT = "your-endpoint.iot.us-east-1.amazonaws.com"
+CLIENT_ID = "Factory_Simulator"
+PATH_TO_CERT = "certs/certificate.pem.crt"
+PATH_TO_KEY = "certs/private.pem.key"
+PATH_TO_ROOT = "certs/AmazonRootCA1.pem"
+
+Delay_Between_Cycles = 5  # in seconds
+Jammed_Probability = 0.05
